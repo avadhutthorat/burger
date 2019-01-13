99cd1586af528f565cc83ab82dc550c4e42cef7a
avadhutthorat <avadhutthorat40@gmail.com>
2019-01-13 15:20:08 +0530

    Updates

---
+++ precache-manifest.715a2cb2a80b45dbcca977d11f09a28e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "34f07b83362b215c2776",
+    "url": "/burger/static/css/main.21722435.chunk.css"
+  },
+  {
+    "revision": "34f07b83362b215c2776",
+    "url": "/burger/static/js/main.34f07b83.chunk.js"
+  },
+  {
+    "revision": "e5d5c6e62a91a02026a5",
+    "url": "/burger/static/js/1.e5d5c6e6.chunk.js"
+  },
+  {
+    "revision": "e4f707eb8257d40b61ad",
+    "url": "/burger/static/js/runtime~main.e4f707eb.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "75bd1bc660d10bc56aa92afd8f9b69be",
+    "url": "/burger/index.html"
+  }
+];
